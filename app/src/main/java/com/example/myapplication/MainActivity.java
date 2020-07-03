a094a5ab5c79e6d8d8f0d18374a06b949bc4c525
skyscape-HXY <2585406486@qq.com>
2020-07-03 17:07:45 +0800

    沉浸式状态栏

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,14 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //如果状态栏只是单纯的颜色
-        StatusBarUtil.setRootViewFitsSystemWindows(this,true);
-        StatusBarUtil.setTranslucentStatus(this);
         StatusBarUtil.setStatusBarColor(this, Color.parseColor("#FF9800"));
 
-        //如果状态栏是图片或者视频
-        StatusBarUtil.setRootViewFitsSystemWindows(this,false);
-        StatusBarUtil.setTranslucentStatus(this);
-        //注意：需要在xml文件中设置StatusBarHeightView
-
+        //图片
+//        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
+//        StatusBarUtil.setTranslucentStatus(this);
     }
 }
